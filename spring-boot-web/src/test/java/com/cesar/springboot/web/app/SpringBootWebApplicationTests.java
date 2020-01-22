1686e6e1e31a45e88ab4e3e864d9c3a8f19fe314
cesarJesus <cgutierrez@na-at.com.mx>
2020-01-22 14:23:50 -0600

    project creation

---
+++ spring-boot-web/src/test/java/com/cesar/springboot/web/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
