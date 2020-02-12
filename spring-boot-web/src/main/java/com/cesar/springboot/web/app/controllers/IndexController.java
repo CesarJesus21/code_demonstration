124f6da09f58e72a23aea7c7e54429c40a192d58
cesarJesus <cesar_jgg@outlook.com>
2020-02-12 01:42:32 -0600

    list of users

--- spring-boot-web/src/main/java/com/cesar/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/cesar/springboot/web/app/controllers/IndexController.java
@@ -3,9 +3,14 @@
  */
 package com.cesar.springboot.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cesar.springboot.web.app.models.User;
 
 /**
  * @author Cesar Gutierrez
@@ -20,4 +25,27 @@ public class IndexController {
 		return "index";
 	}
  
+	
+	
+	@GetMapping("/perfil")
+	public String perfil(Model model){
+		User user = new User();
+		user.setName("Cesar Jesus");
+		user.setLastName("Gutierrez");
+		model.addAttribute("title", "Hello "+user.getName()+"! :D ");
+		model.addAttribute("user", user);
+		return "perfil";
+	}
+	
+
+	@GetMapping("/list")
+	public String list(Model model){
+		List<User> users = new ArrayList<>();
+		users.add(new User("Cesar Jesus", "Gutierrez", "cgutierrez@na-at.com"));
+		users.add(new User("Pablo", "Suarez", "psuarez@na-at.com"));
+		users.add(new User("Andres", "Torres", "atorres@na-at.com"));
+		model.addAttribute("title", "Lista de usuarios");
+		model.addAttribute("users", users);
+		return "list";
+	}
 }
